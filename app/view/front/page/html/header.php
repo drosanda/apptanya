a91d48b27023403dae03de1c8499f75525c353c6
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-12 23:29:59 +0700

    FEAT: ganti ikon

--- app/view/front/page/html/header.php
+++ app/view/front/page/html/header.php
@@ -2,7 +2,7 @@
   <div>
     <a href="<?=base_url()?>">AppTanya</a>
     <a href="#">&nbsp;</a>
-    <a href="<?=base_url('notifikasi')?>"><i class="fa fa-bell-o"></i><sup id="notif_count"></sup></a>
+    <a href="<?=base_url('notifikasi')?>"><i class="fa fa-bell"></i><sup id="notif_count"></sup></a>
   </div>
 </div>
 <div class="gritter-message" style="display: none;"></div>
