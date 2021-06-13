530510b8f0b2c0a5258c889e4425460f217e7745
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-13 12:22:31 +0700

    FIX: pencarian on nginx

--- app/view/front/cari/home.php
+++ app/view/front/cari/home.php
@@ -2,7 +2,7 @@
   <div class="column">
     <div class="content">
       <h1 class="text-center">Cari pertanyaan / Jawaban</h1>
-      <form id="fcari" method="get" action="<?=base_url('cari')?>" class="form-horizontal">
+      <form id="fcari" method="get" action="<?=base_url('cari/')?>" class="form-horizontal">
         <div class="form-group">
           <input id="ikeyword" type="text" class="form-control" name="keyword" placeholder="cth: dimana beli lele" minlength="1" required />
         </div>
