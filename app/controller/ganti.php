b7df00dc2edec3b7ca00ba7d22a375c1927d307b
Ruli Royana Widura <ruliroyanawidura@gmail.com>
2021-06-14 09:44:47 +0700

    FEAT: Ganti Password

---
+++ app/controller/ganti.php
@@ -0,0 +1,28 @@
+<?php
+class Ganti extends JI_Controller
+{
+  public function __construct()
+  {
+    parent::__construct();
+    $this->setTheme('front');
+  }
+  public function index()
+  {
+    $data = $this->__init();
+
+    if($this->user_login){
+      redir(base_url('profil'));
+      die();
+    }
+
+    $this->setTitle('Ganti '.$this->config->semevar->site_suffix);
+    $this->setDescription("Silakan login untuk bisa bertanya atau menjawab di ".$this->config->semevar->site_name);
+    $this->setKeyword('Ganti');
+
+    $this->putThemeContent("ganti/home",$data); //pass data to view
+    $this->putJsContent("ganti/home_bottom",$data); //pass data to view
+
+    $this->loadLayout("col-1",$data);
+    $this->render();
+  }
+}
---
+++ app/view/front/ganti/home.php
@@ -0,0 +1,25 @@
+<div class="section">
+  <div class="column">
+    <div class="content">
+      <h1 class="text-center">Form Ganti Password</h1>
+      <form id="flogin" method="post" class="form-horizontal">
+        <div class="form-group">
+          <label for="iusername" class="control-label">Username *</label>
+          <input id="iusername" type="text" class="form-control" name="username" placeholder="Username" minlength="3" required />
+        </div>
+        <div class="form-group">
+          <label for="ipassword_lama" class="control-label">Password Lama *</label>
+          <input id="ipassword_lama" type="password_lama" class="form-control" name="password_lama" placeholder="password lama" required />
+        </div>
+        <div class="form-group">
+          <label for="ipassword_baru" class="control-label">Password Baru*</label>
+          <input id="ipassword_baru" type="password_baru" class="form-control" name="password_baru" placeholder="password baru" required />
+        </div>
+        <div class="form-group form-action">
+          <button type="submit" class="btn btn-primary btn-submit">Konfirmasi <i class="icon-submit fa fa-sign-in"></i></button>
+          <a href="<?=base_url("lupa")?>" class="btn btn-danger">Batal <i class="fa fa-times" aria-hidden="true"></i></a>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
