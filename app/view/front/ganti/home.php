292577d98a0e4ff8d0ba7a3b0418ec2db0de4589
Ruli Royana Widura <ruliroyanawidura@gmail.com>
2021-06-14 10:02:25 +0700

    FEAT: Ganti password

--- app/view/front/ganti/home.php
+++ app/view/front/ganti/home.php
@@ -4,16 +4,16 @@
       <h1 class="text-center">Reset Passwrod</h1>
       <form id="flogin" method="post" class="form-horizontal">
         <div class="form-group">
-          <label for="iusername" class="control-label">Username *</label>
-          <input id="iusername" type="text" class="form-control" name="username" placeholder="Username" minlength="3" required />
-        </div>
-        <div class="form-group">
           <label for="ipassword_lama" class="control-label">Password Lama *</label>
-          <input id="ipassword_lama" type="password_lama" class="form-control" name="password_lama" placeholder="password lama" required />
+          <input id="ipassword_lama" type="text" class="form-control" name="passwoed_lama" placeholder="password lama" minlength="3" required />
         </div>
         <div class="form-group">
-          <label for="ipassword_baru" class="control-label">Password Baru*</label>
+          <label for="ipassword_baru" class="control-label">Password Baru *</label>
           <input id="ipassword_baru" type="password_baru" class="form-control" name="password_baru" placeholder="password baru" required />
+        </div>
+        <div class="form-group">
+          <label for="iverifikasi_password_baru" class="control-label">Verifikasi Password Baru*</label>
+          <input id="iverifikasi_password_baru" type="verifikasi_password_baru" class="form-control" name="verifikasi_password_baru" placeholder="verifikasi password baru" required />
         </div>
         <div class="form-group form-action">
           <button type="submit" class="btn btn-primary btn-submit">Konfirmasi <i class="icon-submit fa fa-sign-in"></i></button>
